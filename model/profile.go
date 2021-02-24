d57b2e0724727709367786933fb81ed5a339d5cc
Franklin Collin Tamboto <tamboto2000@gmail.com>
2021-02-24 10:12:19 +0700

    Delete Profile model

--- model/profile.go
+++
@@ -1,9 +0,0 @@
-package model
-
-import "github.com/tamboto2000/facebook"
-
-// Profile contains profile data and errors on retrieving data
-type Profile struct {
-	Errors []string `json:"errors"`
-	*facebook.Profile
-}
